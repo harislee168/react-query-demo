14d499286c18938046c1a92379a05c517efe62e4
Haris Lee <haris.lee168@gmail.com>
2023-07-25 11:54:55 +0800

    simple query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import {Routes, Route} from 'react-router-dom'
+import {QueryClientProvider, QueryClient} from 'react-query'
 import { HomePage } from './components/Home.page';
 import { SuperHeroesPage } from './components/SuperHeroes.page';
 import { RQSuperHeroePage } from './components/RQSuperHeroes.page';
 import { NavBar } from './components/NavBar';
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <div>
-        <NavBar />
+        <div>
+          <NavBar />
+        </div>
+        <div className='main-body'>
+          <Routes>
+            <Route path='/' element={<HomePage />}></Route>
+            <Route path='super-heroes' element={<SuperHeroesPage />}></Route>
+            <Route path='rq-super-heroes' element={<RQSuperHeroePage />}></Route>
+          </Routes>
+        </div>
       </div>
-      <div className='main-body'>
-        <Routes>
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='super-heroes' element={<SuperHeroesPage />}></Route>
-          <Route path='rq-super-heroes' element={<RQSuperHeroePage />}></Route>
-        </Routes>
-      </div>
-    </div>
+    </QueryClientProvider>
+
   );
 }
 
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import {useQuery} from 'react-query'
+import axios from 'axios'
 
+
+const superQuery = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
 export const RQSuperHeroePage = () => {
+  const results = useQuery('super-heroes', superQuery)
+
+  let content;
+  console.log(results)
+  if (results.isLoading) {
+    content = <h2>Loading...</h2>
+  }
+  else {
+    if (results.data.data.length) {
+      content = results.data.data.map(data => {return <div key={data.id}>{data.name}</div>})
+    }
+    else if (results.error) {
+      content = results.error.message
+    }
+    else {
+      return <div>No data is available</div>
+    }
+  }
   return (
-    <div>RQSuperHeroes.page</div>
+    <div>{content}</div>
   )
 }
